cde9c72262c558f316846aeccadf220df46dd244
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-04-17 02:32:11 +0500

    Creat Word Counter

---
+++ word_counter.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+// import inquirer library for take from users of input
+import inquirer from "inquirer";
+// Importing the chalk library to add color and styling to terminal output
+import chalk from "chalk";
+
+console.log(chalk.bgMagenta.italic("\t\t Welcom to Word Counter"));
+// declare a constant 'answer' and assign it to the result of inquirer.prompt function
+
+const answer: { sentence: string } = await inquirer.prompt({
+  name: "sentence",
+  type: "input",
+  message: chalk.yellow("Enter your sentence to count the word."),
+});
+
+// "Split the sentence into words using the split method and trim any excess whitespace."
+let words = answer.sentence.trim().split(" ");
+
+// "Print the list of words after splitting the sentence."
+console.log(words);
+
+// print  length of sentence
+console.log(
+  chalk.magenta.italic("your sentence word count is  "),
+  words.length
+);
